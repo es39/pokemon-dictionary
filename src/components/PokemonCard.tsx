e1222cc458ef9fbbeb91349f2a7b3b838c5e485e
es39 <esj10025@gmail.com>
2023-05-31 19:49:58 +0900

    :sparkles: Feat : Pokemon Card add

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,5 +1,41 @@
+import pokeAxios from 'api/pokeAxios';
+import { useEffect, useState } from 'react';
+
 const PokemonCard = () => {
-  return <div>포켓몬 카드 컴포넌트</div>;
+  const [pokemon, setPokemon] = useState([]);
+  const axiosNum = 10;
+
+  // console.log(res.data.names[2].name): 한국 이름
+  const getPokemonList = () => {
+    pokeAxios
+      .get(`/pokemon?limit=${axiosNum}`)
+      .then(res => {
+        console.log(res.data);
+        setPokemon(res.data.results);
+      })
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    getPokemonList();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {pokemon.map((el: any, index: number) => (
+          <li key={index}>{el.name}</li>
+        ))}
+      </ul>
+      {/* <img src={pokemon} /> */}
+    </div>
+  );
 };
 
 export default PokemonCard;
+
+/* FIXME: 
+1. pokemon/1(id) : 해당 포켓몬 정보
+2. pokemon/1(id)/sprites : 해당 포켓몬 이미지
+3. pokemon-species/1(id)/names[2].name : 한국이름
+*/
