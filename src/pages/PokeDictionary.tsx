22c5e92c146feef3fdaf319e0e4b172b71b74b8d
es39 <esj10025@gmail.com>
2023-05-31 17:36:13 +0900

    :sparkles: Feat : pokemon list

--- src/pages/PokeDictionary.tsx
+++ src/pages/PokeDictionary.tsx
@@ -1,5 +1,33 @@
+import pokeAxios from 'api/pokeAxios';
+import { useEffect, useState } from 'react';
+
 const PokeDictionary = () => {
-  return <div>포켓몬 도감</div>;
+  const [pokemon, setPokemon] = useState([]);
+  const axiosNum = 100;
+
+  // console.log(res.data.names[2].name): 한국 이름
+  const getPokemonList = () => {
+    pokeAxios
+      .get(`/pokemon?limit=${axiosNum}`)
+      .then(res => {
+        console.log(res.data);
+        setPokemon(res.data.results);
+      })
+      .catch(err => console.log(err));
+  };
+  useEffect(() => {
+    getPokemonList();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {pokemon.map((el: any, index: number /* el interface 설정하기 */) => (
+          <li key={index}>{el.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default PokeDictionary;
