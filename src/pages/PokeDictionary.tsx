0c5d245739d5c67e398355e87a3d81036dd6e4ea
es39 <esj10025@gmail.com>
2023-05-31 18:54:38 +0900

    :bulb: Comment : 주석 추가

--- src/pages/PokeDictionary.tsx
+++ src/pages/PokeDictionary.tsx
@@ -22,12 +22,19 @@ const PokeDictionary = () => {
   return (
     <div>
       <ul>
-        {pokemon.map((el: any, index: number /* el interface 설정하기 */) => (
+        {pokemon.map((el: any, index: number) => (
           <li key={index}>{el.name}</li>
         ))}
       </ul>
+      {/* <img src={pokemon} /> */}
     </div>
   );
 };
 
 export default PokeDictionary;
+
+/* FIXME: 
+1. pokemon/1(id) : 해당 포켓몬 정보
+2. pokemon/1(id)/sprites : 해당 포켓몬 이미지
+3. pokemon-species/1(id)/names[2].name : 한국이름
+*/
