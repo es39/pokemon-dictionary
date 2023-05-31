37824b2a43e08621154c562cfaf43864fd3c68ad
es39 <esj10025@gmail.com>
2023-05-31 19:43:08 +0900

    :lipstick: Design : 위치 설정

--- src/pages/PokeDictionary.tsx
+++ src/pages/PokeDictionary.tsx
@@ -1,40 +1,33 @@
-import pokeAxios from 'api/pokeAxios';
-import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 const PokeDictionary = () => {
-  const [pokemon, setPokemon] = useState([]);
-  const axiosNum = 100;
-
-  // console.log(res.data.names[2].name): 한국 이름
-  const getPokemonList = () => {
-    pokeAxios
-      .get(`/pokemon?limit=${axiosNum}`)
-      .then(res => {
-        console.log(res.data);
-        setPokemon(res.data.results);
-      })
-      .catch(err => console.log(err));
-  };
-  useEffect(() => {
-    getPokemonList();
-  }, []);
-
   return (
-    <div>
-      <ul>
-        {pokemon.map((el: any, index: number) => (
-          <li key={index}>{el.name}</li>
-        ))}
-      </ul>
-      {/* <img src={pokemon} /> */}
-    </div>
+    <Container>
+      <SearchBar>검색창</SearchBar>
+      <PokemonCards>카드 띄울 공간</PokemonCards>
+    </Container>
   );
 };
 
 export default PokeDictionary;
 
-/* FIXME: 
-1. pokemon/1(id) : 해당 포켓몬 정보
-2. pokemon/1(id)/sprites : 해당 포켓몬 이미지
-3. pokemon-species/1(id)/names[2].name : 한국이름
-*/
+const Container = styled.main`
+  width: 100vw;
+  height: 100vh;
+  background-color: purple;
+  flex-direction: column;
+  justify-content: start;
+`;
+
+const SearchBar = styled.section`
+  display: flex;
+  background-color: white;
+  width: 100vw;
+  height: 50px;
+`;
+
+const PokemonCards = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
