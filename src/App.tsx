745218099292d7e5fe681a807f416dcc9477b21b
es39 <esj10025@gmail.com>
2023-05-31 17:33:51 +0900

    :recycle: Refactor : code modify

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Footer, Header } from 'containers';
 import PokeDictionary from 'pages/PokeDictionary';
 import TypeDictionary from 'pages/TypeDictionary';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
@@ -16,7 +15,6 @@ const App = () => {
           <Route path="/typedic" element={<TypeDictionary />} />
           <Route path="/genedic" element={<TypeDictionary />} />
         </Routes>
-        <ToastContainer />
       </main>
       <Footer />
     </BrowserRouter>
