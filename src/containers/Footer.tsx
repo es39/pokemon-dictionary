2c9d3389064a21910b7a61aded200bf3377d276a
es39 <esj10025@gmail.com>
2023-06-01 17:12:08 +0900

    :lipstick: Design : Footer design modify

--- src/containers/Footer.tsx
+++ src/containers/Footer.tsx
@@ -1,5 +1,13 @@
+import styled from 'styled-components';
+
 const Footer = () => {
-  return <div>footer</div>;
+  return <FooterStyle></FooterStyle>;
 };
 
 export default Footer;
+
+const FooterStyle = styled.footer`
+  width: 100vw;
+  height: 150px;
+  background-color: blue;
+`;
