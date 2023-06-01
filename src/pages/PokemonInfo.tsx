600e24fd35ecbece147fb98b01d0956177d98501
es39 <esj10025@gmail.com>
2023-06-01 18:58:30 +0900

    :lipstick: Design : 포켓몬 정보 보여줄 페이지 스타일 수정

--- src/pages/PokemonInfo.tsx
+++ src/pages/PokemonInfo.tsx
@@ -1,5 +1,18 @@
+import styled from 'styled-components';
+
 const PokemonInfo = () => {
-  return <div>포켓몬 정보 띄워줄 페이지</div>;
+  return (
+    <Container>
+      포켓몬 정보 띄워줄 페이지포켓몬 정보 띄워줄 페이지포켓몬 정보 띄워줄
+      페이지포켓몬 정보 띄워줄 페이지포켓몬 정보 띄워줄 페이지포켓몬 정보 띄워줄
+      페이지포켓몬 정보 띄워줄 페이지포켓몬 정보 띄워줄 페이지포켓몬 정보 띄워줄
+      페이지포켓몬 정보 띄워줄 페이지
+    </Container>
+  );
 };
 
 export default PokemonInfo;
+
+const Container = styled.main`
+  width: 100vw;
+`;
