c2699b3828a4ba34f8af7d29211ac8f48fbaa0c8
es39 <esj10025@gmail.com>
2023-06-02 16:00:39 +0900

    :sparkles: Feat : 포켓몬 타입 인터페이스 추가

--- src/interfaces/Pokemon.interface.tsx
+++ src/interfaces/Pokemon.interface.tsx
@@ -10,18 +10,27 @@ interface pokemonImg {
   front_shiny: string;
 }
 
-// interface pokemonType {
-//   types: types[];
-// }
-
-// interface types {
-//   slot: number;
-//   type: type[];
-// }
-
 interface type {
-  name: string;
-  url: string;
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
 }
 
-export type { pokemonName, pokemonImg, type };
+interface pokeSprites {
+  back_default: string;
+  back_shiny: string;
+  front_default: string;
+  front_shiny: string;
+  other: {
+    dream_world: {
+      front_default: string;
+    };
+    'official-artwork': {
+      front_default: string;
+    };
+  };
+}
+
+export type { pokemonName, pokemonImg, type, pokeSprites };
