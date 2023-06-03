94e95cc7ec9704be8d28860be893ad8eb6b32600
es39 <esj10025@gmail.com>
2023-06-03 13:44:20 +0900

    :lipstick: Design : icon import and style modify

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -4,7 +4,9 @@ import styled from 'styled-components';
 const Header = () => {
   return (
     <HeaderStyle>
-      <FiMenu />
+      <IconWrapper>
+        <MenuIcon />
+      </IconWrapper>
     </HeaderStyle>
   );
 };
@@ -12,9 +14,25 @@ const Header = () => {
 export default Header;
 
 const HeaderStyle = styled.header`
-  width: 100vw;
+  width: 100%;
   height: 60px;
-  background: red;
+  background: #ff5656;
   box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.25);
   position: fixed;
+`;
+
+const IconWrapper = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  padding: 10px;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+`;
+
+const MenuIcon = styled(FiMenu)`
+  font-size: 2rem;
+  cursor: pointer;
 `;
